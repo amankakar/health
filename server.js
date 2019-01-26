62effc37e439e5e9f980565535459d866c3dac51
amankakar <aman1017@gmail.com>
2019-01-26 18:50:09 +0500

    heroku fix

--- server.js
+++ server.js
@@ -18,15 +18,16 @@ const next = require("next");
 const routes = require("./routes");
 const app = next({ dev: process.env.NODE_ENV !== "production" });
 const handler = routes.getRequestHandler(app);
-
+var server_port = process.env.YOUR_PORT || process.env.PORT || 3000;
+var server_host = process.env.YOUR_HOST || "0.0.0.0";
 // With express
 const express = require("express");
 
 app.prepare().then(() => {
   express()
     .use(handler)
-    .listen(3000, err => {
+    .listen(server_port, server_host, err => {
       if (err) throw err;
-      console.log("Ready on localhost 3000");
+      console.log(server_port);
     });
 });
